5019867d114c7a39ecdc95d2a34435aede1ba902
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-09 11:11:36 +0200

    initial commit

---
+++ inspired_plus/src/Pages/Courses.jsx
@@ -0,0 +1,10 @@
+function Courses() {
+  return (
+    <div style={{ padding: "120px 20px" }}>
+      <h1>Courses</h1>
+      <p>All training courses will appear here.</p>
+    </div>
+  );
+}
+
+export default Courses;
