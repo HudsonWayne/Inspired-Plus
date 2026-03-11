fdae3198a99e76c77c1e3c65fefd45dc14c5d1a5
HudsonWayne <waynewaynebenhura@gmail.com>
2026-03-11 15:36:50 +0200

    initial commit

--- inspired_plus/src/Pages/Courses.jsx
+++ inspired_plus/src/Pages/Courses.jsx
@@ -1,4 +1,3 @@
-// src/Pages/Courses.jsx
 import React from "react";
 import "./Courses.css";
 
@@ -28,6 +27,15 @@ export default function Courses() {
             This nationally recognised qualification prepares current and aspiring managers to operate in line with the requirements of The Children’s Homes (England) Regulations 2015. 
             Through this programme, learners develop the essential safeguarding, leadership, and operational skills needed to deliver safe, compliant, and high-quality care services for children and young people.
           </p>
+        </div>
+      </section>
+
+      {/* OPTIONAL CTA BUTTON */}
+      <section className="courses-cta">
+        <div className="container">
+          <a href="/contact/employer-get-in-touch" className="cta-button">
+            Get in Touch
+          </a>
         </div>
       </section>
     </div>
